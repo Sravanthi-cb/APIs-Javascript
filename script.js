b6d03b4b8daade4d2126a119e338e02e12502ea2
Sravanthi <sravanthi.cb@gmail.com>
2017-08-03 19:55:25 -0400

    modified

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// HTTP
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
+
+// Parsing XML
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.codecademy.com/files/samples/javascript_learn_apis.xml", false);
 
@@ -6,3 +15,9 @@ xhr.send();
 
 xmlDocument = xhr.responseXML;
 console.log(xmlDocument.childNodes['0'].textContent);
+
+//Parsing JSON
+var demo = '{"pets": { "name": "Jeffrey", "species": "Giraffe"}}';
+
+var json = JSON.parse(demo);
+console.log(json);
